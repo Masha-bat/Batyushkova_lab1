f83101037d53d4c0cc6b71088623cc960d00c4a7
Masha-bat <batyushkova03@mail.ru>
2022-09-20 13:23:05 +0300

    Lab1 update1

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct pipe 
+struct Pipe 
 {
     float length = 0, diametr = 0;
     bool repair = 0;
@@ -14,10 +14,54 @@ struct CS
     float efficiency = 0;
 };
 
-
+void 
 
 int main()
 {
-    cout << "Menu \n1. Add pipe \n2. Add CS \n3. View all objects \n4. Edit pipe \n5. Edit CS \n6. Save \n7. Download \n0. Exit \n";
+    int action = -1;
+    Pipe p;
+    CS cs;
+
+    while (action)
+    {
+        cout << "Menu \n1. Add pipe \n2. Add CS \n3. View all objects \n4. Edit pipe \n5. Edit CS \n6. Save \n7. Download \n0. Exit \n";
+        cin >> action;
+        switch (action)
+        {
+        case1:
+            {
+                addPipe();
+                break;
+            }
+        case2:
+            {
+                addCS();
+                break;
+            }
+        case3:
+            {
+                viewObjects();
+                break;
+            }
+        case4:
+            {
+
+            }
+        case5:
+            {
+
+            }
+        case6:
+            {
+
+            }
+        case7:
+            {
+            
+            }
+
+        }
+    }
+    
 }
 
